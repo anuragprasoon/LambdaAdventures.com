1f04bc879cc3ac89105017957f365c4b9e4200b5
anuragprasoon <anuragprasoon23@gmail.com>
2025-05-22 22:03:17 +0530

    meta image

--- src/pages/trekking.tsx
+++ src/pages/trekking.tsx
@@ -6,9 +6,13 @@ export default function trekking() {
     return(
         <>
         <Head>
-        <title>Lambda Adventures - Our Story </title>
-        <meta name="description" content=" Lambda Adventures - India's trusted trekking and trip planning company. Experience expertly guided treks, personalized service, and unforgettable adventures across India. Join us for safe, authentic, and value-packed journeys that inspire and connect. Your next adventure starts here!" />
+        <title>Chandratal Trek with Hampta Pass | Lambda Adventures </title>
+        <meta name="description" content="Experience the Chandratal Lake trek from Manali with LambdaAdventure.com, a popular trekking route in Himachal Pradesh. Starting from Jobra, the trek passes through Chika, Balu ka Ghera, and Hampta Pass, leading to Chandratal Lake at 14,100 feet. Enjoy river crossings, mountain views, and organized support from guides and staff for a safe and memorable trekking experience in the Himalayas." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:image" content="https://cdn.builder.io/api/v1/image/assets/4b5665cbbffd4ab3b47a4640b783bd87/f38bfb124d56e3ab1c6d7c6027988e0ecc4ebedc" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content="Chandratal Lake Trek from Manali in Himachal Pradesh with LambdaAdventure.com" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <TrekAbout/>
