587ba0c777f91d9d97903c236ee9389df564356d
anuragprasoon <anuragprasoon23@gmail.com>
2025-06-13 18:04:13 +0530

    data input style update

--- src/components/CustomForm.tsx
+++ src/components/CustomForm.tsx
@@ -13,7 +13,7 @@ const CustomTrip:React.FC =()=>{
                     <input type="text" className="outline-none  border-b-1 border-b-[#017C6D] mb-3 p-2 block" placeholder="No. of Travellers"/>
                     <div className="pt-3 pb-3">
                         <label className="text-[14px] block mb-2">Starting Date</label> 
-                        <input type="date" className="outline-none  border-b-1 border-b-[#017C6D] mb-3 p-2" placeholder="Trip Starting Date"/>
+                        <input type="date" className="outline-1 outline-[#017C6D] mb-3 p-2" placeholder="Trip Starting Date"/>
                     </div>
                     <div className="flex gap-5">
                         <input type="number" className="outline-none  border-b-1 border-b-[#017C6D] mb-3 p-2 w-[120px] text-[14px]" placeholder="No. of Days"/>
